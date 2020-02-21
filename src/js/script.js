3e0d81a0bee7a93faad15a90b30843cfd32e7518
Nursultan <nmukhamedali@gmail.com>
2020-02-21 10:42:01 +0600

    Изменил Бургер меню

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,4 @@
 $(document).ready(function(){
-    $(".hamburger").click(function (e) {
-        e.preventDefault();
-        $('.active__menu').fadeIn();
-    });
-
-    $(".close-btn").click(function (e) {
-        e.preventDefault();
-        $('.active__menu').fadeOut();
-    });
     $('.close_navbar').on('click', function(){
         $('.active__menu').fadeOut();
     });
@@ -34,3 +25,37 @@ $(window).scroll(function(){
         }
     }
 });
+
+$('.hamburger').click(function(){
+	$(this).toggleClass('open');
+	var active = $(this).toggleClass('')[0].getAttribute('class');
+	(active == 'hamburger open') ? menu(1) : menu(0) 
+});
+
+function menu ( e ) {
+	e = (e == 1) ? 0 : 1;
+	var arr = [
+		//hamburger
+		[50, 0],
+		[50, 100],
+		[45, 0],
+	]
+	TweenMax.to($('.line:nth-child(1)'), 0.25, {top: arr[0][e] + '%', rotation: arr[2][e]});
+	TweenMax.to($('.line:nth-child(2)'), 0.25, {scale: e});
+	TweenMax.to($('.line:nth-child(3)'), 0.25, {top: arr[1][e] + '%', rotation: -arr[2][e]});
+}
+
+
+$('document').ready(function(){
+    $('.hamburger').click(function(){
+    if ($(".active__menu").is(":visible") == true) {
+    $('.active__menu').hide();
+    }
+    else
+    {
+    $('.active__menu').show();
+    }
+    });
+});
+
+        
